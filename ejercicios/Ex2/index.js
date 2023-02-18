fb9093db969e42553d32ce9b78a2b4784f3ae37a
FedeNicoletti <federiconicoletti@outlook.com>
2023-02-18 07:37:15 -0300

    Ejercicio2

---
+++ ejercicios/Ex2/index.js
@@ -0,0 +1,10 @@
+let miNombre = "Federico",
+  edad = 23,
+  isDeveloper = true,
+  dateOfBirth = new Date("September 25 1999"),
+  libroFavorito = {
+    titulo: "Juicio Final",
+    autor: "John Katzenbach",
+    "fecha-de-edicion": new Date("march 2016"),
+    url: "https://es.wikipedia.org/wiki/Juicio_final_(novela)",
+  };
