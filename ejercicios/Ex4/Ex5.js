261c4c40e8f0a2e07d45124e111ad79425c20f2f
FedeNicoletti <federiconicoletti@outlook.com>
2023-02-24 03:08:34 -0300

    Ex5

---
+++ ejercicios/Ex4/Ex5.js
@@ -0,0 +1,8 @@
+let alturacm = 185,
+alturam = 1.85,
+pesokg = 85.5,
+alturamroundedup = Math.ceil(alturam),
+pesokgroundeddown = Math.floor(pesokg),
+maxvalue = Number.MAX_VALUE;
+maxvalueplusone = maxvalue + 1;
+console.log(Number.MAX_VALUE + 1 == Number.MAX_VALUE);
