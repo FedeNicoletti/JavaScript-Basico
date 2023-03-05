e0fec735c24272bb513bf4aab3f8d67658be84cc
FedeNicoletti <federiconicoletti@outlook.com>
2023-03-05 00:11:47 -0300

    Ex6

--- ejercicios/Ex6/Ex6.js
+++ ejercicios/Ex6/Ex6.js
@@ -1,42 +1,35 @@
-const compra = ["Leche", "Huevos", "Mantequilla", "Miel", "Yogures"]
+let compra = ["Tomate", "Huevo", "Lechuga", "Leche", "Yogurt"];
 
-compra.push("Aceite de Girasol")
-compra.pop()
+compra.push("Aceite de Girasol");
+compra.pop();
 
-const peliculas = [
-    {
-        titulo: "El efecto mariposa",
-        director: "Eric Bress",
-        fecha: new Date(2004, 3, 30)
-    },
-    {
-        titulo: "Spider-Man: No Way Home",
-        director: "Jon Watts",
-        fecha: new Date(2021, 11, 16)
-    },
-    {
-        titulo: "Pulp Fiction",
-        director: "Quentin Tarantino",
-        fecha: new Date(1995, 0, 13)
-    }
-]
+let peliculas = [
+  {
+    titulo: "Avatar 2",
+    director: "James Cameron",
+    fecha: new Date(2023, 11, 15),
+  },
+  {
+    titulo: "La tormenta perfecta",
+    director: "Wolfgang Petersen",
+    fecha: new Date(2000, 7, 3),
+  },
+  {
+    titulo: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    fecha: new Date(1995, 0, 13),
+  },
+];
 
-const peliculasNuevas = peliculas.filter(pelicula => pelicula.fecha > new Date(2010, 0, 1))
+let peliculasFiltradas = peliculas.filter(
+  (pelicula) => pelicula.fecha > new Date(2010, 0, 1)
+);
 
-const directores = peliculas.map(pelicula => {
-    return pelicula.director
-})
-const titulos = peliculas.map(pelicula => {
-    return pelicula.titulo
-})
-const directores_titulos = directores.concat(titulos)
-const directores_titulos_prop = [...directores, ...titulos]
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-
-
+let directores = peliculas.map((pelicula) => {
+  return pelicula.director;
+});
+let titulos = peliculas.map((pelicula) => {
+  return pelicula.titulo;
+});
+let directores_titulos = directores.concat(titulos);
+let directores_titulos_prop = [...directores, ...titulos];
