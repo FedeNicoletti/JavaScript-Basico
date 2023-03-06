b767f329d6a5205694a384ae406b8d955cc34717
FedeNicoletti <federiconicoletti@outlook.com>
2023-03-06 11:01:12 -0300

    Ex12

--- ejercicios/Ex12/index.js
+++ ejercicios/Ex12/index.js
@@ -1,11 +1,11 @@
 const logger = require('./logger')
 
-logger.info("Hola soy un mensaje de info")
-logger.warn("Hola soy un mensaje de warn")
-logger.error("Hola soy un mensaje de error")
+logger.info("Hola soy un mensaje de info");
+logger.warn("Hola soy un mensaje de warn");
+logger.error("Hola soy un mensaje de error");
 
 try {
-    
+
 } catch (error) {
-    logger.error("Hola soy un mensaje de error")
+    logger.error("Hola soy un mensaje de error");
 }
