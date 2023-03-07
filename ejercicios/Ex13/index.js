cb1212858cc2fb75ffd535cbc9a87d2334ffd548
FedeNicoletti <federiconicoletti@outlook.com>
2023-03-07 15:42:33 -0300

    Ex13

---
+++ ejercicios/Ex13/index.js
@@ -0,0 +1,13 @@
+/* 
+- Modifica el último console.log del entrypoint index.js para devolver el resultado en color verde */
+import {suma, multiplica} from './controller.js';
+import {Chalk} from 'chalk';
+
+const chalk = new Chalk({level: 1});
+const log = console.log;
+
+const sum = suma(1,2);
+const mult = multiplica(4,5);
+console.log(sum);
+log(chalk.green(mult));
+
